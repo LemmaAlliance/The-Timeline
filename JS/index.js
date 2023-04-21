35c2ca6446cade2d490d34e1798731625b25107d
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-04-21 07:37:26 +0100

    Got line working & scaling
    also started on css

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,13 @@
 let zoomFactor = 1;
+const scrnWidth = screen.width;
+const scrnHeight = window.innerHeight;
+
+function clamp(min, num){
+  if(num < min){
+    num = min;
+  }
+  return(num);
+}
 
 document.addEventListener("wheel", function(event) {
   event.preventDefault(); // Prevents the default scrolling behavior
@@ -12,6 +21,16 @@ document.addEventListener("wheel", function(event) {
     zoomFactor -= 0.1;
   }
 
+  zoomFactor = clamp(1, zoomFactor);
+
   // Set the scale transform on the element
   document.getElementById("my-element").style.transform = `scaleX(${zoomFactor})`;
+}, { passive: false }); // Add the { passive: false } option
+window.addEventListener("load", function() {
+  console.log("Page has finished loading");
+  // Do something else here
+  document.getElementById("my-element").style.width = scrnWidth;
+  console.log(scrnHeight/2);
+  document.getElementById("my-element").style.marginTop = (scrnHeight/2) + "px";
+  document.getElementById("my-element").style.marginBottom = 0;
 });
