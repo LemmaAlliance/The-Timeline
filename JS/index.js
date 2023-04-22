977adc87eb28a128a616949ee6a68115ee0d05a6
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-04-22 16:54:10 +0000

    Attempted to make it go side to side

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 let zoomFactor = 1;
 const scrnWidth = screen.width;
 const scrnHeight = window.innerHeight;
+const myElement = document.getElementById("my-element");
+let xPos = 0;
 
 function clamp(min, num){
   if(num < min){
@@ -24,13 +26,30 @@ document.addEventListener("wheel", function(event) {
   zoomFactor = clamp(1, zoomFactor);
 
   // Set the scale transform on the element
-  document.getElementById("my-element").style.transform = `scaleX(${zoomFactor})`;
+  myElement.style.transform = `scaleX(${zoomFactor})`;
 }, { passive: false }); // Add the { passive: false } option
+
+window.addEventListener("keydown", function (e){
+  console.log(`You pressed ${e.key}`);
+  if(e.key == "ArrowLeft"){
+    xPos += -10
+  };
+  if(e.key == "ArrowRight"){
+    xPos += 10
+  };
+  if(xPos < 0){
+    myElement.style.marginRight = -(xPos + "%");
+  };
+  if(xPos > 0){
+    myElement.style.marginLeft = (xPos + "%");
+  };
+}, false);
+
 window.addEventListener("load", function() {
   console.log("Page has finished loading");
   // Do something else here
-  document.getElementById("my-element").style.width = scrnWidth;
+  myElement.style.width = scrnWidth;
   console.log(scrnHeight/2);
-  document.getElementById("my-element").style.marginTop = (scrnHeight/2) + "px";
-  document.getElementById("my-element").style.marginBottom = 0;
+  myElement.style.marginTop = (scrnHeight/2) + "px";
+  myElement.style.marginBottom = 0;
 });
