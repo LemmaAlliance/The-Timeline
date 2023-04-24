35cee3d8b73fdc299bddf84d8394f2cd1ca1dfd7
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-04-24 16:45:45 +0100

    Update

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 let zoomFactor = 1;
-const scrnWidth = screen.width;
+const scrnWidth = window.innerWidth;
 const scrnHeight = window.innerHeight;
 const myElement = document.getElementById("my-element");
 const hi = document.getElementById("Hi");
@@ -22,7 +22,7 @@ function clamp(min, num){
 }
 
 function refresh(){
-  hi.style.left = (pix+20)+"px";
+  hi.style.left = (percen)+"%";
 }
 
 document.addEventListener("wheel", function(event) {
@@ -39,8 +39,12 @@ document.addEventListener("wheel", function(event) {
 
   zoomFactor = clamp(1, zoomFactor);
 
+  myElement.style.margin = "20,20";
+
   // Set the scale transform on the element
-  myElement.style.transform = `scaleX(${zoomFactor})`;
+  myElement.style.width = ((scrnWidth*zoomFactor)-40)+"px";
+  console.log("Screen width + zoom factor: "+((scrnWidth*zoomFactor)-40));
+  refresh();
 }, { passive: false }); // Add the { passive: false } option
 
 window.addEventListener("keydown", function (e){
