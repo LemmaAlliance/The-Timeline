9acdb1b0d8dce55a6954a214b558cc963c4aedc8
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2023-05-10 13:11:08 +0100

    Deleted hard-coded files

--- JS/testing.js
+++
@@ -1,9 +0,0 @@
-let items = [{"id":"Jan 28 2010 00:00:00", "element":"myElement"}, {"id":"Jan 30 2010 00:00:00", "element":"myElement2"}];
-let dates = [];
-
-dates.push({"date":new Date(items[0].id), "element":items[0]});
-for (var i = 0; i < items.length; i++) {
-    dates.push({"date":new Date(items[i].id), "element":items[i]});
-    console.log(`Item ${i}'s date is: ${dates[0].date}`);
-    //Do something
-}
